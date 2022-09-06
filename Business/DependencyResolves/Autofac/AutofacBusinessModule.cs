6411d4f9e72514b0d5b2405f27bb5bdccf4922d3
cbozbuga41@gmail.com <cbozbuga41@gmail.com>
2022-09-06 16:29:01 +0300

    14th

--- Business/DependencyResolves/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolves/Autofac/AutofacBusinessModule.cs
@@ -1,7 +1,11 @@
 ﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.CCS;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Entities.Utilities.Interceptors;
+using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using System;
@@ -20,7 +24,19 @@ namespace Business.DependencyResolves.Autofac
             builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
             builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
 
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
 
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
 
             /* builder.RegisterType<FileLogger>().As<ILogger>().SingleInstance();*/ //Biri senden ILogger isterse arka planda ona FileLogger ver.
         }
